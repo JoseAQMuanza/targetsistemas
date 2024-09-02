c2ac9881162494bfd53d6e4dbd4e1ffdb9261482
JoseAQMuanza <108295929+JoseAQMuanza@users.noreply.github.com>
2024-09-02 21:38:32 +0100

    Create exercicio-4.py

---
+++ exercicio-4.py
@@ -0,0 +1,3 @@
+ # Primeiro eu tentaria descobria o interuptor que controla a minha lampada, com isso eu ia ter 
+ # que focar apenas nos outros dois depois acharia o interuptor da sala B e com isso o meu
+ # problema estaria resolvido 
